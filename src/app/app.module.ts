cb652db18eed979721a972d934e220a663aef5a0
Brandon Lood <31374750+bslood@users.noreply.github.com>
2018-04-20 14:49:39 -0400

    Merge branch 'master' into bslood

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,8 @@ import { RestListPage } from '../pages/rest-list/rest-list';
 import { ResDetailPage } from '../pages/res-detail/res-detail';
 import { FavPage } from '../pages/fav/fav';
 import { FavDetailPage } from '../pages/fav-detail/fav-detail';
-import { WheelPage } from '../pages/wheel/wheel';
 import { TabsPage } from '../pages/tabs/tabs';
-import { HelpPage } from '../pages/help/help'; 
+import { HelpPage } from '../pages/help/help';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -28,7 +27,6 @@ import { Data } from '../providers/data';
     ResDetailPage,
     FavPage,
     FavDetailPage,
-    WheelPage,
     HelpPage
   ],
   imports: [
@@ -46,7 +44,6 @@ import { Data } from '../providers/data';
     ResDetailPage,
     FavPage,
     FavDetailPage,
-    WheelPage,
     HelpPage
   ],
   providers: [
