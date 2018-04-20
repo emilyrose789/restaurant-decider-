f71c1148464676e1c64388ee2ac4ff4c35f664dd
Brandon Lood <31374750+bslood@users.noreply.github.com>
2018-04-20 14:49:05 -0400

    Merge branch 'master' into bslood

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
 import { RestListPage } from '../rest-list/rest-list';
 import { FavPage } from '../fav/fav';
-import { WheelPage } from '../wheel/wheel';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = RestListPage;
-  tab2Root = FavPage;
-  tab3Root = WheelPage;
+  tab1Root = FavPage;
+  tab2Root = RestListPage;
 
   constructor() {
 
